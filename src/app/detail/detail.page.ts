67f80e66d8f5f59867eb090980d3f78a526021a9
kevzid <a3524110558@alumno.uttehuacan.edu.mx>
2026-03-03 23:33:28 -0600

    detalle ejercicio

---
+++ src/app/detail/detail.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail',
+  templateUrl: './detail.page.html',
+  styleUrls: ['./detail.page.scss'],
+  standalone: false
+})
+export class DetailPage {
+  constructor() {}
+}
